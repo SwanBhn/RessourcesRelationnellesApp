4f67e1b3d9ccd591ffc99f8e9434bcc55c32824e
Martin <martin.frontera85@gmail.com>
2024-05-22 14:01:21 +0200

    ✨page profil

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -6,7 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['tab4.page.scss']
 })
 export class Tab4Page {
+  isLoginFormVisible: boolean = true;
+  showPassword: boolean = false;
 
   constructor() {}
 
+  toggleForm() {
+    this.isLoginFormVisible = !this.isLoginFormVisible;
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
 }
